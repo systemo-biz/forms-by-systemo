49141759a0ccc5d2aa439a8d0cb311b28fa602ac
yumashev <a.yumashev@gmail.com>
2014-08-30 19:37:12 +0600

    refactoring spam protect

--- cp_calbackform.php
+++ cp_calbackform.php
@@ -8,47 +8,9 @@
 
 
  include_once('includes/emailer.php');
+ include_once('includes/spam_protect.php');
 
-/*
-Получает код для защиты формы от спама
 
-Если произошел клик по кнопке, то считаем что это человек и заполняем скрытое поле.
-
-Затем функция проверит наличие этого поля и если оно есть, то пропускаем, иначе помечаем как спам.
-*/
-function get_spam_protect_html($type_spam_protect){
-
-	if(empty($type_spam_protect)) return;
-
-	ob_start();
-	?>
-	<input type="hidden" class="spam_protect" name="meta_data_form_cp[spam_protect]">
-	<script type="text/javascript">
-		(function($) {
-			$("input[type='submit']").click(function () {
-      			$('.spam_protect').val('hs');
-    		});
-		})(jQuery);
-	</script>
-	<?php
-	$html = ob_get_contents();
-	ob_end_clean();
-
-	return $html;
-}
-
-add_action('added_post_meta', 'check_spam_form_cp', 10, 4);
-add_action('updated_post_meta', 'check_spam_form_cp', 10, 4);
-function check_spam_form_cp($mid, $object_id, $meta_key, $meta_value) {
-	
-	//проверяем чтобы это была мета которая содержит отметку о спаме
-	if($meta_key != 'meta_spam_protect') return;
-
-	if($meta_value != 'hs') {
-		wp_trash_post($object_id);
-	}
-
-}
 
 ////// шорт код вызова формы + обработчик данных из формы
 add_shortcode( 'form-cp', 'cpform_func' );
@@ -287,7 +249,7 @@ function form_message_add_post_type_cp() {
 		'show_in_menu'        => true,
 		'show_in_nav_menus'   => true,
 		'show_in_admin_bar'   => true,
-		'menu_position'       => 5,
+		'menu_position'       => 55,
 		'can_export'          => true,
 		'has_archive'         => true,
 		'exclude_from_search' => false,
