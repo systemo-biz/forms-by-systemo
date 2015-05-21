e320845127424ed57b5e06d0e21d1f3c60f9e257
Anatoly Yumashev <yumashev@users.noreply.github.com>
2015-05-22 02:16:16 +0500

    Change readme

--- forms-s.php
+++ forms-s.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Forms by Systemo
 Description: Calback Form
-Version: 20150522
+Version: 20150522.2
 GitHub Plugin https://github.com/systemo-biz/forms-cp
 GitHub Branch: master
 Author: http://systemo.org
