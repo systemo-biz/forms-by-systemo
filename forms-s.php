5a60bc0d3b6535fa1fad86953ac4a7c5731dc928
yumashev <a.yumashev@gmail.com>
2015-06-09 12:44:43 +0500

    chg version

--- forms-s.php
+++ forms-s.php
@@ -11,7 +11,7 @@ Text Domain:       forms_s
 Domain Path:       /languages
 GitHub Plugin URI: https://github.com/systemo-biz/forms-s
 GitHub Branch: master
-Version:           20150607.4
+Version:           20150609
 */
 
 
