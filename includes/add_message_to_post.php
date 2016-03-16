93e70ca583d36eb06dcd81ad4dc178fb73ade4b9
Vitaliy Max <vm@casepress.org>
2016-03-16 13:02:57 +0500

    Добавленна возможность сохранять UTM метки в мету сообщений. Удобный бокс с настройками,поддерживающий любое колличество новых настроек, нужно просто добавить html код настройки в бокс.

--- includes/add_message_to_post.php
+++ includes/add_message_to_post.php
@@ -1,6 +1,8 @@
 <?php
 add_action('init', 'add_message_to_posts');
 function add_message_to_posts(){
+	//записываем UTM метки в COOKIE если они есть
+	set_cookie_s($_GET);
 	// проверяем пустая ли data_cp
 	if(empty($_REQUEST['data_form_cp'])) return;
 	$data_form = $_REQUEST['data_form_cp']; // если не пустая то записываем значения для  проверки существованя
@@ -28,7 +30,12 @@ function add_message_to_posts(){
 	foreach($meta_data_form as $key => $value):
 		add_post_meta($post_id, 'meta_' . $key, $value);
 	endforeach;
-    
+
+	//записываем utm метки в мету поста если это включенно в настройках шаблона
+	if (get_post_meta($template_post_id, 's_utm_m', 1) == 1) {
+		set_meta_utm_s($_COOKIE, $post_id);
+	}
+
 	$content_data = '';
 	//Шаблон уведомления
 	$notice_template_data=get_post_meta($template_post_id,'notice_template', true);
@@ -45,15 +52,43 @@ function add_message_to_posts(){
 	endforeach;
     //Заполнение по шаблону уведомления
     if(preg_match_all('|\[\[(.+)\]\]|isU',$notice_template_data,$arr)){
-		for ($i=0; $i < count($arr[0]) ; $i++) { 
+		for ($i=0; $i < count($arr[0]) ; $i++) {
 			$notice_template_data=str_replace($arr[0][$i], get_post_meta($post_id,$arr[1][$i],true), $notice_template_data);
 		}
 	}
 	$content_data.=$notice_template_data;
 
 	$post_data = array(
-		'ID' => $post_id, 
+		'ID' => $post_id,
 		'post_content' => $content_data,
 		);
 	wp_update_post( $post_data );
+}
+// функция записи UTM меток в COOKIE
+function set_cookie_s($get_s) {
+	foreach ($get_s as $key => $value) {
+		$utm_s = strpos($key, 'utm');
+		$gclid_s = strpos($key, 'gclid');
+		if (!($utm_s === false)) {
+		    setcookie( $key, $value, time() + 3600, COOKIEPATH, COOKIE_DOMAIN );
+		}
+		if (!($gclid_s === false)) {
+		    setcookie( $key, $value, time() + 3600, COOKIEPATH, COOKIE_DOMAIN );
+		}
+	}
+	return;
+}
+// функция записи UTM меток в мету поста
+function set_meta_utm_s($cookie_s, $post_id) {
+	foreach ($cookie_s as $key => $value) {
+		$utm_s = strpos($key, 'utm');
+		$gclid_s = strpos($key, 'gclid');
+		if (!($utm_s === false)) {
+		    add_post_meta($post_id, 'meta_' . $key, $value);
+		}
+		if (!($gclid_s === false)) {
+		    add_post_meta($post_id, 'meta_' . $key, $value);
+		}
+	}
+	return;
 }
